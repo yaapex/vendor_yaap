fc9cdcfb21094d5328a32b30389ca5fd9744f105
xenxynon <valkyrie.xps@gmail.com>
2026-03-26 17:51:56 +0000

    Revert "config: Fix material expressive for permission manager"
    This reverts commit 7c9c810694d34141f6e92da6f856b6c8a4eef6df.

--- config/common.mk
+++ config/common.mk
@@ -151,10 +151,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.input.video_enabled=false
 
-# Enable Material Design 3 Expressive
-PRODUCT_PRODUCT_PROPERTIES += \
-    is_expressive_design_enabled=true
-
 # Packages
 include vendor/yaap/config/packages.mk
 
