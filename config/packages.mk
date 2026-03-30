72b5dfbe2251c2b76131e4f50ab73d0bc682c87e
Cyber Knight <cyberknight755@gmail.com>
2026-03-30 18:21:53 +0800

    yaap: Drop CLO telephony packages
    These packages should not be built on other platforms as they cause bizarre issues with APNs, thus they are moved to hardware/qcom-caf/common.
    
    Change-Id: I9364745c5c5a8c491cd39347bce5ac319ae641ea
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -109,26 +109,3 @@ PRODUCT_PACKAGES += \
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
-
-# Telephony - CLO
-PRODUCT_PACKAGES += \
-    extphonelib \
-    extphonelib-product \
-    extphonelib.xml \
-    extphonelib_product.xml \
-    ims-ext-common \
-    ims_ext_common.xml \
-    tcmiface \
-    telephony-ext \
-    qti-telephony-hidl-wrapper \
-    qti-telephony-hidl-wrapper-prd \
-    qti_telephony_hidl_wrapper.xml \
-    qti_telephony_hidl_wrapper_prd.xml \
-    qti-telephony-utils \
-    qti-telephony-utils-prd \
-    qti_telephony_utils.xml \
-    qti_telephony_utils_prd.xml
-
-PRODUCT_BOOT_JARS += \
-    tcmiface \
-    telephony-ext
